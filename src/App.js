84ba493c2d3ff66dc6f30bacf2389bbd389c877f
BatturM <battur_maidar@brown.edu>
2022-05-21 00:41:00 +0800

    Added props

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ import Card from './components/Card'
 export default function App() {
   return (
     <div className="App">
-      <Navbar /> 
+      <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img="https://i.imgur.com/IPzO03F.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}/>
     </div>
-  );
+  )
 }
+
 
